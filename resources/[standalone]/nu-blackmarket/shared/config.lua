5e1e07118c6dc01fd1197291a9fd791dd817a4bd
cstone1995 <xdreamzclan@gmail.com>
2025-11-06 22:43:45 +0000

    d

--- resources/[standalone]/nu-blackmarket/shared/config.lua
+++ resources/[standalone]/nu-blackmarket/shared/config.lua
@@ -7,7 +7,7 @@ Config.Debug = true
 -- Ped Configuration
 Config.Ped = {
     model = `g_m_m_chigoon_01`, -- Black market dealer ped model
-    coords = vector4(-75.0, -818.0, 327.5, 70.0), -- Default location (can be changed)
+    coords = vector4(1400.3207, 3641.8157, 27.9513, 225.4040), -- Default location (can be changed)
     scenario = "WORLD_HUMAN_SMOKING", -- Ped animation/scenario
     freeze = true, -- Should the ped be frozen in place
     invincible = true, -- Should the ped be invincible

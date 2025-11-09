2a84b1dacd97fb6b953efde5685384e2f544f9f5
cstone1995 <xdreamzclan@gmail.com>
2025-11-09 23:05:56 +0000

    Update get_framework_config.lua

--- resources/jpr-phonesystem/configs/get_framework_config.lua
+++ resources/jpr-phonesystem/configs/get_framework_config.lua
@@ -1,12 +1,15 @@
--- Framework bridge for Qbox / QBCore compatibility
+-- Framework detection bridge for Qbox and QB-Core
+
+local Framework = nil
 
 if GetResourceState('qbx_core') == 'started' then
-    Framework = exports['qbx_core']:GetCoreObject()
+    -- Use Qbox's global core (does not need export)
+    Framework = exports['qbx_core'] and exports['qbx_core']:GetCoreObject and exports['qbx_core']:GetCoreObject() or QBX
 elseif GetResourceState('qb-core') == 'started' then
     Framework = exports['qb-core']:GetCoreObject()
 else
-    print("^1[JPR Phone] Could not find any active core framework (qbx_core or qb-core).")
-    Framework = nil
+    print('^1[JPR Phone] No valid framework found (qbx_core or qb-core). Using fallback mode.^0')
+    Framework = {}
 end
 
 return Framework

a00037a4604b096fcbd2f6432a4efe6efac823e0
cstone1995 <xdreamzclan@gmail.com>
2025-11-09 22:58:28 +0000

    s

--- resources/jpr-phonesystem/configs/get_framework_config.lua
+++ resources/jpr-phonesystem/configs/get_framework_config.lua
@@ -1,1 +1,12 @@
-QBCore = exports[Config.CoreName]:GetCoreObject()
+-- Framework bridge for Qbox / QBCore compatibility
+
+if GetResourceState('qbx_core') == 'started' then
+    Framework = exports['qbx_core']:GetCoreObject()
+elseif GetResourceState('qb-core') == 'started' then
+    Framework = exports['qb-core']:GetCoreObject()
+else
+    print("^1[JPR Phone] Could not find any active core framework (qbx_core or qb-core).")
+    Framework = nil
+end
+
+return Framework

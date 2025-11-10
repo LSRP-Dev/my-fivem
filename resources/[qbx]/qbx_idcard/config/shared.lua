a10559d65b42f532095852590fc0f67b44f14e17
cstone1995 <xdreamzclan@gmail.com>
2025-11-10 17:23:37 +0000

    s

--- resources/[qbx]/qbx_idcard/config/shared.lua
+++ resources/[qbx]/qbx_idcard/config/shared.lua
@@ -21,6 +21,42 @@ return {
             backgroundImage = 'https://i.ibb.co/vxvGzg1/card.png',
             prop = 'prop_franklin_dl',
         },
+        ['driver_bike'] = {
+            header = 'Bike License',
+            background = '#febbbb',
+            backgroundImage = 'https://i.ibb.co/vxvGzg1/card.png',
+            prop = 'prop_franklin_dl',
+        },
+        ['driver_car'] = {
+            header = 'Car License',
+            background = '#febbbb',
+            backgroundImage = 'https://i.ibb.co/vxvGzg1/card.png',
+            prop = 'prop_franklin_dl',
+        },
+        ['driver_truck'] = {
+            header = 'Truck License',
+            background = '#febbbb',
+            backgroundImage = 'https://i.ibb.co/vxvGzg1/card.png',
+            prop = 'prop_franklin_dl',
+        },
+        ['driver_boat'] = {
+            header = 'Boat License',
+            background = '#febbbb',
+            backgroundImage = 'https://i.ibb.co/vxvGzg1/card.png',
+            prop = 'prop_franklin_dl',
+        },
+        ['driver_helicopter'] = {
+            header = 'Helicopter License',
+            background = '#febbbb',
+            backgroundImage = 'https://i.ibb.co/vxvGzg1/card.png',
+            prop = 'prop_franklin_dl',
+        },
+        ['driver_plane'] = {
+            header = 'Plane License',
+            background = '#febbbb',
+            backgroundImage = 'https://i.ibb.co/vxvGzg1/card.png',
+            prop = 'prop_franklin_dl',
+        },
         ['weaponlicense'] = {
             header = 'Weapon License',
             background = '#c7ffe5',

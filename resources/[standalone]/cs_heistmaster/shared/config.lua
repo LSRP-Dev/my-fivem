42f7f611dbfa9a553c3b8cab27fb1dbf99d4fd8a
cstone1995 <xdreamzclan@gmail.com>
2025-11-17 21:07:26 +0000

    d

--- resources/[standalone]/cs_heistmaster/shared/config.lua
+++ resources/[standalone]/cs_heistmaster/shared/config.lua
@@ -84,7 +84,8 @@ Config.Heists = {
         start = { x = 148.9, y = -1040.1, z = 29.37 },
 
         vault = {
-            coords = { x = 147.8602, y = -1045.1481, z = 29.3647 },  -- Adjusted coordinates - moved from wall
+            coords = { x = 147.034, y = -1046.515, z = 29.367 },
+        },  -- Adjusted coordinates - moved from wall
             heading = 250.0,  -- Closed position - aligns door flush with wall (vanilla interior)
             doorModel = 'v_ilev_gb_vauldr',  -- NEW: the vault door model to spawn
         },

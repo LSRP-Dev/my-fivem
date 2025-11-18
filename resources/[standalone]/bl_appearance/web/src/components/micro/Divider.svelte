0717f044c31afacf20aff980ad422ced708008c1
cstone1995 <xdreamzclan@gmail.com>
2025-11-18 15:52:50 +0000

    d

--- resources/[standalone]/bl_appearance/web/src/components/micro/Divider.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    import { scale } from 'svelte/transition';
-</script>
-
-<div
-    transition:scale|global={{ delay: 1000 }}
-    class="w-full h-[0.2vh] bg-primary/20 {$$props.class}"
-></div>

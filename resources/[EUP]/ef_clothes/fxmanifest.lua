3bd590b8000863e4e0a3f53662ab8e870b90be9c
cstone1995 <xdreamzclan@gmail.com>
2025-11-20 21:01:56 +0000

    d

--- resources/[EUP]/ef_clothes/fxmanifest.lua
+++ resources/[EUP]/ef_clothes/fxmanifest.lua
@@ -1,12 +1,10 @@
--- Generated with AltTool
-
 fx_version 'cerulean'
-game { 'gta5' }
+game 'gta5'
 
 files {
-  'mp_m_freemode_01_mp_m_np_new.meta',
-  'mp_f_freemode_01_mp_f_np_new.meta'
+  "mp_f_freemode_01_np_shop.meta",
+  "mp_m_freemode_01_np_shop.meta"
 }
 
-data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_np_new.meta'
-data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_np_new.meta'
+data_file "SHOP_PED_APPAREL_META_FILE" "mp_f_freemode_01_np_shop.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "mp_m_freemode_01_np_shop.meta"

eb0fa0e185253b13d94ca97fc7f039b902619d1d
cstone1995 <xdreamzclan@gmail.com>
2025-11-28 15:59:59 +0000

    s

--- resources/[EUP]/Male-EUP/fxmanifest.lua
+++ resources/[EUP]/Male-EUP/fxmanifest.lua
@@ -1,7 +1,17 @@
 fx_version 'cerulean'
 
-games   { 'gta5' }
+game 'gta5'
 
-description 'Clothing'
+description 'Male EUP Clothing'
 
 lua54 'yes'
+
+files {
+    'mp_f_freemode_01_mp_f_ensart_sonsof.meta',
+    'mp_m_freemode_01_mp_m_ensart_sonsof.meta',
+    'pedalternatevariations.meta'
+}
+
+data_file 'PED_ALTERNATE_VARIATIONS_FILE' 'pedalternatevariations.meta'
+data_file 'PED_METADATA_FILE' 'mp_f_freemode_01_mp_f_ensart_sonsof.meta'
+data_file 'PED_METADATA_FILE' 'mp_m_freemode_01_mp_m_ensart_sonsof.meta'
